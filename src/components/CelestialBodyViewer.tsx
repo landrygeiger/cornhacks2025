64d6968d4ac43670d14df18e336875b7798fd473
Landry Geiger <landry.geiger@gmail.com>
2025-03-01 14:06:57 -0600

    Remove unused import

--- src/components/CelestialBodyViewer.tsx
+++ src/components/CelestialBodyViewer.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef, useState } from "react";
+import { FC, useRef, useState } from "react";
 import Camera from "./Camera";
 import { CelestialBody } from "../types/celestial-body";
 
