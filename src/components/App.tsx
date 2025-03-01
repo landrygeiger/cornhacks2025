835247fa2b0649df0c55e31ca39ebb028c91fa74
Landry Geiger <landry.geiger@gmail.com>
2025-03-01 12:26:10 -0600

    Display fullscreen camera

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,7 @@ import { FC } from "react";
 import Camera from "./Camera";
 
 const App: FC = () => {
-  return (
-    <>
-      <Camera width={20} height={20} />
-    </>
-  );
+  return <Camera />;
 };
 
 export default App;
