5f6cc19fea0a38a4c6233bbae59ff04735045475
Landry Geiger <landry.geiger@gmail.com>
2025-03-01 22:25:24 -0600

    Remove unused imports

--- src/components/CelestialBodyViewer.tsx
+++ src/components/CelestialBodyViewer.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef, useState } from "react";
+import { FC, useEffect, useRef } from "react";
 import * as THREE from "three";
 import { CelestialBody } from "../types/celestial-body";
 import { DeviceOrientationControls } from "../utils/DeviceOrientationControls";
